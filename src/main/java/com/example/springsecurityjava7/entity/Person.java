3f40db077162f16ec8c74a0fe1c250caa28b23b6
Mont1k <nurtilekorozov@gmail.com>
2022-12-01 12:19:44 +0600

    s

--- src/main/java/com/example/springsecurityjava7/entity/Person.java
+++ src/main/java/com/example/springsecurityjava7/entity/Person.java
@@ -4,10 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static jakarta.persistence.CascadeType.ALL;
 
@@ -16,7 +17,7 @@ import static jakarta.persistence.CascadeType.ALL;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Person {
+public class Person implements UserDetails {
     @Id
     @SequenceGenerator(name="person_seq",sequenceName = "person_seq",allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "person_seq")
@@ -34,4 +35,38 @@ public class Person {
     }
     @ManyToMany(cascade = ALL,fetch = FetchType.EAGER,mappedBy = "personList")
     private Set<Role> roles = new HashSet<>();
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = getRoles();
+        List<SimpleGrantedAuthority>authorities = new ArrayList<>();
+        for (Role role:roles){
+            authorities.add(new SimpleGrantedAuthority(role.getName()))
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
